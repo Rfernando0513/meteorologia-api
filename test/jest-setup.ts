c4b8289b00bfdeda0ef378065e6a876f66ad8d06
Rfernando0513 <rfernando0513@gmail.com>
2024-08-11 11:14:25 -0300

    finish  route and primary test, add pretier

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -1,10 +1,10 @@
 import { SetupServer } from '@src/server';
 import supertest, { SuperTest, Test } from 'supertest';
 
-beforeAll(async () => {
-    console.log('Setting up test environment...');
-    const server = new SetupServer();
-    await server.init();
-    global.testRequest = supertest(server.getApp()) as unknown as SuperTest<Test>;
-    console.log('Test environment setup complete.');
+beforeAll(() => {
+  console.log('Setting up test environment...');
+  const server = new SetupServer();
+  server.init();
+  global.testRequest = supertest(server.getApp()) as unknown as SuperTest<Test>;
+  console.log('Test environment setup complete.');
 });
